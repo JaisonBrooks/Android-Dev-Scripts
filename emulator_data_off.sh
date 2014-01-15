1a77be240ef540962e3cb7f17965f2f84adc5974
Jaison Brooks <jaisonbrooks@gmail.com>
2014-01-15 02:31:39 -0700

    Update emulator_data_off.sh

--- emulator_data_off.sh
+++ emulator_data_off.sh
@@ -5,6 +5,6 @@ spawn telnet localhost 5554
 expect "OK"
 send "gsm data off\n"
 expect "OK"
-#send "gsm voice off"
-#expect "OK"
 send "quit\n"
+
+#You can also use {send "gsm voice off" expect "OK" } to turn just voice off
